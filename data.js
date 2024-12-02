c17259fa9f5d11295de1c5e4918c345f91b25a28
FlannersHimself <102362210+FlannersHimself@users.noreply.github.com>
2024-12-02 09:22:42 -0400

    360

---
+++ data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-20241127_185340000_ios",
+      "name": "20241127_185340000_iOS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": -2.8852344521287847,
+        "pitch": 0.09282735491038885,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.1918045293770234,
+          "pitch": -0.29337020171984207,
+          "title": "Title",
+          "text": "<span class=\"tagnamecolor\" style=\"box-sizing: inherit; color: brown; font-family: Consolas, Menlo, &quot;courier new&quot;, monospace; font-size: 15px;\"><span class=\"tagcolor\" style=\"box-sizing: inherit; color: mediumblue;\">&lt;</span>a<span class=\"attributecolor\" style=\"box-sizing: inherit; color: red;\">&nbsp;href<span class=\"attributevaluecolor\" style=\"box-sizing: inherit; color: mediumblue;\">=\"<span style=\"color: rgb(255, 255, 255); font-family: Helvetica, Arial, sans-serif; font-size: 13px;\">https://www.smu.ca/</span>\"</span></span><span class=\"tagcolor\" style=\"box-sizing: inherit; color: mediumblue;\">&gt;</span></span><i style=\"box-sizing: inherit; color: rgb(0, 0, 0); font-family: Consolas, Menlo, &quot;courier new&quot;, monospace; font-size: 15px;\">link text</i><span class=\"tagnamecolor\" style=\"box-sizing: inherit; color: brown; font-family: Consolas, Menlo, &quot;courier new&quot;, monospace; font-size: 15px;\"><span class=\"tagcolor\" style=\"box-sizing: inherit; color: mediumblue;\">&lt;</span>/a<span class=\"tagcolor\" style=\"box-sizing: inherit; color: mediumblue;\">&gt;&nbsp;</span></span>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
